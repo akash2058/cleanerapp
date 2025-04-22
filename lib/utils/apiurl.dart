24849d23a1de6161f03cb96b32b44ce7db9c3684
akash2058 <aakashsharma73235@gmail.com>
2025-04-22 07:25:39 +0545

    api implementation of the booking update

--- lib/utils/apiurl.dart
+++ lib/utils/apiurl.dart
@@ -1,6 +1,6 @@
 class AppUrl {
   static const mainUrl =
-      'https://99ae-2400-1a00-b030-a171-8486-fe60-1255-9984.ngrok-free.app/api/';
+      ' https://9fdb-2404-7c00-41-695d-d586-6190-b4fa-c1c4.ngrok-free.app/api/';
   // static const mainUrl = 'http://127.0.0.1:8000/api/';
   static const loginurl = '${mainUrl}login';
   static const binbooking = '${mainUrl}bin-bookings';
