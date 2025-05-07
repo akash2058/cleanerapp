0cc7ac82600806c3f3a13cea8739dfd3f42eb723
akash2058 <aakashsharma73235@gmail.com>
2025-05-07 18:47:31 +0545

    create no search found

--- lib/view/dashboard_screens/home/bin_search_bar/bin_search_bar_view.dart
+++ lib/view/dashboard_screens/home/bin_search_bar/bin_search_bar_view.dart
@@ -1,5 +1,6 @@
 import 'package:binbookingapp/custom_widget/cleaner_textfield.dart';
 import 'package:binbookingapp/utils/appcolors.dart';
+import 'package:binbookingapp/utils/cleanericonspng.dart';
 import 'package:binbookingapp/utils/style.dart';
 import 'package:binbookingapp/view/authentication/login/login_provider/login_provider.dart';
 import 'package:binbookingapp/view/dashboard_screens/bin_request/bin_request_provider/bin_request_provider.dart';
@@ -18,47 +19,84 @@ class BinSearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => BinRequestProvider()..setRequests(
-        model.data.siteRequests, model.data.warehouseRequests),
-      child: Consumer<LoginProvider>(builder:(context, log, child) {
-        return Scaffold(
-        appBar: AppBar(
-          title: Text("Search Requests",style: appbartitlefont,),
-        ),
-        body: Consumer<BinRequestProvider>(
-          builder: (context, provider, _) {
-            return Padding(
-              padding:  EdgeInsets.symmetric(horizontal: 20,vertical: 10).r,
-              child: Column(
-                spacing: 20.r,
-                children: [
-                  CleanerTextfield(
-                    fillColor: CleanerAppcolors.primaryWhitecolor,
-                    hintlabel: 'Search',prefix: Icon(Icons.search_outlined),),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: provider.filteredRequests.length,
-                      itemBuilder: (context, index) {
-                        final item = provider.filteredRequests[index];
-                        return BinRequestCard(
-                          onPressed: () {
-                            showModalBottomSheet(context: context, builder: (context) {
-                              return BinBookingBottomSheet(customername: item.customerName, location: item.location, endDate: item.endDate, type: item.type, binsizeName: item.binSizeName, bookingId: item.id.toString(), userId: log.userid, );
-                            },);
-                          },
-                          address: item.location, quantity: item.quantity.toString(), startdate: item.startDate, binsizename: item.binSizeName, duration: item.orderDuration.toString());
-                      },
-                    ),
-                  )
-                ],
+      create:
+          (_) =>
+              BinRequestProvider()..setRequests(
+                model.data.siteRequests,
+                model.data.warehouseRequests,
               ),
-            );
-          },
-        ),
-      );
-      }, )
+      child: Consumer<LoginProvider>(
+        builder: (context, log, child) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Search Requests", style: appbartitlefont),
+            ),
+            body: Consumer<BinRequestProvider>(
+              builder: (context, provider, _) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10).r,
+                  child: Column(
+                    spacing: 20.r,
+                    children: [
+                      CleanerTextfield(
+                        fillColor: CleanerAppcolors.primaryWhitecolor,
+                        hintlabel: 'Search',
+                        prefix: Icon(Icons.search_outlined),
+                      ),
+                      Expanded(
+                        child:
+                            provider.filteredRequests.isEmpty
+                                ? Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Image.asset(AppIcons.closedd,height: 70.r,),
+                                      Text(
+                                        'No suggestions found',
+                                        style: resendfont
+                                      ),
+                                    ],
+                                  ),
+                                )
+                                : ListView.builder(
+                                  itemCount: provider.filteredRequests.length,
+                                  itemBuilder: (context, index) {
+                                    final item =
+                                        provider.filteredRequests[index];
+                                    return BinRequestCard(
+                                      onPressed: () {
+                                        showModalBottomSheet(
+                                          context: context,
+                                          builder: (context) {
+                                            return BinBookingBottomSheet(
+                                              customername: item.customerName,
+                                              location: item.location,
+                                              endDate: item.endDate,
+                                              type: item.type,
+                                              binsizeName: item.binSizeName,
+                                              bookingId: item.id.toString(),
+                                              userId: log.userid,
+                                            );
+                                          },
+                                        );
+                                      },
+                                      address: item.location,
+                                      quantity: item.quantity.toString(),
+                                      startdate: item.startDate,
+                                      binsizename: item.binSizeName,
+                                      duration: item.orderDuration.toString(),
+                                    );
+                                  },
+                                ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
-
-
