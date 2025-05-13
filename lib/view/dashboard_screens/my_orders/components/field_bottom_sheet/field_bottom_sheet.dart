36900966f832de3c0dc781635e359172983df3f1
akash2058 <aakashsharma73235@gmail.com>
2025-05-13 06:58:06 +0545

    remove unused imports

--- lib/view/dashboard_screens/my_orders/components/field_bottom_sheet/field_bottom_sheet.dart
+++ lib/view/dashboard_screens/my_orders/components/field_bottom_sheet/field_bottom_sheet.dart
@@ -1,7 +1,6 @@
 import 'package:binbookingapp/custom_widget/button.dart';
 import 'package:binbookingapp/custom_widget/custom_tile.dart';
 import 'package:binbookingapp/utils/appcolors.dart';
-import 'package:binbookingapp/utils/form_validation.dart';
 import 'package:binbookingapp/utils/style.dart';
 import 'package:binbookingapp/view/dashboard_screens/my_orders/my_orders_provider/my_order_provider.dart';
 import 'package:flutter/material.dart';
--- lib/view/dashboard_screens/my_orders/detailscreen/my_orders_drop_off_details_screen.dart
+++ lib/view/dashboard_screens/my_orders/detailscreen/my_orders_drop_off_details_screen.dart
@@ -1,7 +1,6 @@
 import 'package:binbookingapp/custom_widget/button.dart';
 import 'package:binbookingapp/custom_widget/transaction_route.dart';
 import 'package:binbookingapp/utils/appcolors.dart';
-import 'package:binbookingapp/utils/form_validation.dart';
 import 'package:binbookingapp/utils/style.dart';
 import 'package:binbookingapp/view/authentication/login/login_provider/login_provider.dart';
 import 'package:binbookingapp/view/dashboard_screens/bin_request/bin_request_provider/bin_request_provider.dart';
