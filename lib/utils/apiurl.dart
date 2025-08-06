062805f695bbdf9d10db05419a0d61bbf40eb0f1
akash2058 <aakashsharma73235@gmail.com>
2025-08-06 21:03:09 +0545

    change new items including customercompany and customer comment

--- lib/utils/apiurl.dart
+++ lib/utils/apiurl.dart
@@ -1,7 +1,7 @@
 class AppUrl {
-//   static const mainUrl =
-//       'http://nextlevelbins.com.au/api/';
-  static const mainUrl = 'https://166d21068409.ngrok-free.app/api/';
+  static const mainUrl =
+      'http://nextlevelbins.com.au/api/';
+  // static const mainUrl = 'https://166d21068409.ngrok-free.app/api/';
   static const loginurl = '${mainUrl}login';
   static const binbooking = '${mainUrl}bin-bookings';
   static const myorder = '${mainUrl}bin-bookings/order?driver_id=';
