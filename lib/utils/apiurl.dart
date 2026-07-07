4d5345397986838ee9cde5ab30e6d7dfdaf218de
Nitin-U <nitinutsav80@gmail.com>
2026-07-07 07:50:36 +0545

    changing api url

--- lib/utils/apiurl.dart
+++ lib/utils/apiurl.dart
@@ -1,6 +1,6 @@
 class AppUrl {
   static const mainUrl =
-      'http://nextlevelbins.com.au/api/';
+      'http://nicebackyardbinhire.com.au/api/';
   // static const mainUrl = 'https://166d21068409.ngrok-free.app/api/';
   static const loginurl = '${mainUrl}login';
   static const binbooking = '${mainUrl}bin-bookings';
